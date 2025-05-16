2e534a21076ce883149e84347116e172939d45e1
Maneesha630 <maneesha.annavaram@gmail.com>
2025-05-16 18:47:48 +0530

    array added

--- array.sh
+++ array.sh
@@ -4,6 +4,6 @@ fruits=("APPLE" "KIWI" "GRAPES")
 
 echo "First fruit is ${fruits[0]}"
 echo " second fruit is ${fruits[2]}"
-
+echo "all fruits are ${fruits[@]}"
 array=("1" "2")
 echo " frist ${array[1]}"
