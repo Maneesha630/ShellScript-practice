81df392aa93b122f60ab937d26b5bf60383985f1
Maneesha630 <maneesha.annavaram@gmail.com>
2025-05-18 16:34:37 +0530

    installation through shell

--- installation.sh
+++ installation.sh
@@ -10,3 +10,18 @@ then
     exit 1
 fi
 dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
+else
+    echo "Git is already installed, nothing to do.."
+fi
